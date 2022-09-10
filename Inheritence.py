a4bef40018f780fe0c148051935e3ec51f5761ff
meghangordon <buffy10626@gmail.com>
2022-09-10 12:04:48 -0700

    step 238 - inheritance submission

---
+++ Inheritence.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    brand = 'No Brand Provided'
+    model = ' '
+    speed = ' '
+    inventory_number = 0
+
+class Car(Vehicle):
+    door = 4
+    engine = 'gasoline'
+
+class Motorcylce(Vehicle):
+    top_speed = ' '
+    weight_limit = '300'
